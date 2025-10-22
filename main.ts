9a9a4fcb0aab05eb8bb530b04efe9bf85e91ecd7
sbheuiser <sbheuiser@gmail.com>
2025-10-22 16:48:29 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,17 @@
 radio.onReceivedNumber(function (receivedNumber) {
     if (receivedNumber == 1) {
         cuteBot.turnleft()
-        cuteBot.stopcar()
-    } else if (false) {
+        cuteBot.motors(0, 30)
+    } else if (receivedNumber == 2) {
         cuteBot.turnright()
-        cuteBot.stopcar()
+        cuteBot.motors(30, 0)
     } else if (receivedNumber == 3) {
         cuteBot.forward()
-        cuteBot.stopcar()
+        cuteBot.motors(30, 30)
     } else if (receivedNumber == 4) {
         cuteBot.stopcar()
-    } else {
-    	
     }
 })
 basic.showIcon(IconNames.Heart)
 radio.setGroup(1)
+music.play(music.builtinPlayableSoundEffect(soundExpression.mysterious), music.PlaybackMode.UntilDone)
